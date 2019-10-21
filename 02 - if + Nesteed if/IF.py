d08f67d911814fc28721bfe25e147a82727cfbc9
husin14 <husinsmg@gmail.com>
2019-10-21 15:23:07 +0700

    Pyshare 2

---
+++ 02 - if + Nesteed if/IF.py
@@ -0,0 +1,11 @@
+#Soal 1
+nilai = int(input("Masukkan nilai = "))
+
+if (nilai < 0):
+    print("Nilai {} kurang dari 0 ".format(nilai))
+
+#Soal 2
+presiden = input("Siapakah presiden indonesia sekarang?? = ")
+
+if (presiden == "jokowi"):
+    print("Benarrr, presiden kita sekarang adlaah jokowi")
